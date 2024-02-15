8ed7aa34281c1c4741f5767075295a9e727e424a
Ryan Atkinson <mail@ryanatkn.com>
2024-02-15 16:22:19 -0700

    wip

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,3 +1,3 @@
 export const prerender = true;
 // TODO BLOCK needed?
-// export const ssr = false; // fixes whitespace pop-in, see https://github.com/sveltejs/svelte/issues/8615
+export const ssr = false; // fixes whitespace pop-in, see https://github.com/sveltejs/svelte/issues/8615
