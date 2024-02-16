7f7dd8eab0fd3a948b6cdf7230af73d96a9b9037
Ryan Atkinson <mail@ryanatkn.com>
2024-02-16 12:18:50 -0700

    wip

--- src/lib/Page_Footer.svelte
+++ src/lib/Page_Footer.svelte
@@ -1,14 +1,17 @@
 <script lang="ts">
+	import type {Url} from '@ryanatkn/gro/paths.js';
+
 	import Breadcrumb from '$lib/Breadcrumb.svelte';
 	import Library_Footer from '$lib/Library_Footer.svelte';
 	import type {Package_Meta} from '$lib/package_meta.js';
 
 	export let pkg: Package_Meta | {url: string; package_json: null};
+	export let root_url: Url | null = null;
 </script>
 
 <footer>
 	{#if pkg.package_json}
-		<Library_Footer {pkg} root_url="https://www.fuz.dev/" />
+		<Library_Footer {pkg} {root_url} />
 	{/if}
 	<slot />
 	<div class="breadcrumb_wrapper">
