90f068e9b0271ecd14e521843b1d7da5e38dbe33
Ryan Atkinson <mail@ryanatkn.com>
2024-02-23 11:43:57 -0700

    wip

--- src/routes/library/colors/Color_Swatch.svelte
+++ src/routes/library/colors/Color_Swatch.svelte
@@ -1,26 +1,31 @@
 <script lang="ts">
-	import {hsl_to_rgb, parse_hsl_string} from '@ryanatkn/belt/colors.js';
+	import {hsl_to_hex_string, hsl_to_rgb, parse_hsl_string} from '@ryanatkn/belt/colors.js';
 
 	export let color_name: string;
 	export let computed_styles: CSSStyleDeclaration;
 
-	const get_color_value = (name: string) => {
+	const get_color_hsl_string = (name: string) => {
 		const v = computed_styles.getPropertyValue('--' + name);
 		return v;
 	};
+
+	// TODO BLOCK responsive
 </script>
 
 <ul>
 	{#each {length: 9} as _, i}
 		{@const num = i + 1}
 		{@const variable_name = `color_${color_name}_${num}`}
-		{@const value = get_color_value(variable_name)}
-		{@const hsl = parse_hsl_string(value)}
+		{@const hsl_string = get_color_hsl_string(variable_name)}
+		{@const hsl = parse_hsl_string(hsl_string)}
 		<li style:--bg_color="var(--{variable_name})">
 			<div class="color"></div>
-			<code>{variable_name}</code>
-			<span>{value}</span>
-			<span>rgb({hsl_to_rgb(...hsl).join(', ')})</span>
+			<div class="text">
+				<div class="name">{variable_name}</div>
+				<div class="hex">{hsl_to_hex_string(...hsl)}</div>
+				<div class="hsl">{hsl_string}</div>
+				<div class="rgb">rgb({hsl_to_rgb(...hsl).join(', ')})</div>
+			</div>
 		</li>
 	{/each}
 </ul>
@@ -32,6 +37,25 @@
 	li {
 		display: flex;
 		align-items: center;
+		font-family: var(--font_family_mono);
+	}
+	.text {
+		display: flex;
+		flex: 1;
+		padding-left: var(--spacing_sm);
+	}
+	.name {
+		width: 100px;
+	}
+	.hex {
+		width: 85px;
+	}
+	.hsl {
+		width: 150px;
+		font-size: var(--size_sm);
+	}
+	.rgb {
+		font-size: var(--size_sm);
 	}
 	.color {
 		height: 32px;
