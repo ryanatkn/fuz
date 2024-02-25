70fc9d6f5dc9094c063ceddd2f9d9d7600f06e9e
Ryan Atkinson <mail@ryanatkn.com>
2024-02-25 08:00:57 -0700

    upgrade fuz_code

--- src/routes/package.ts
+++ src/routes/package.ts
@@ -29,7 +29,7 @@ export const package_json = {
 		'@changesets/changelog-git': '^0.2.0',
 		'@ryanatkn/belt': '^0.20.4',
 		'@ryanatkn/eslint-config': '^0.1.0',
-		'@ryanatkn/fuz_code': '^0.8.0',
+		'@ryanatkn/fuz_code': '^0.9.0',
 		'@ryanatkn/gro': '^0.112.2',
 		'@sveltejs/adapter-static': '^3.0.1',
 		'@sveltejs/kit': '^2.5.1',
