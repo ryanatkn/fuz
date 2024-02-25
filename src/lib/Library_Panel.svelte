bbf53b51ffe00fa3234accf63b157dc2c2bff150
Ryan Atkinson <mail@ryanatkn.com>
2024-02-25 11:22:07 -0800

    layout fixes (#11)

--- src/lib/Library_Panel.svelte
+++ src/lib/Library_Panel.svelte
@@ -5,11 +5,6 @@
 <style>
 	.library_panel {
 		overflow: hidden;
-		width: 100%;
-		display: flex;
-		align-items: flex-start;
-		justify-content: center;
-		flex-direction: column;
 		padding: var(--library_panel_padding, var(--spacing_lg));
 		/* This margin matches the padding between the library menu and main content. */
 		margin-bottom: calc(var(--spacing_md) + var(--spacing_sm));
