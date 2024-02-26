6cc9b97240c1822433fa6ed4b209bcc0deed3680
Ryan Atkinson <mail@ryanatkn.com>
2024-02-26 15:11:18 -0700

    wip

--- src/routes/library/light_and_shadow/+page.svelte
+++ src/routes/library/light_and_shadow/+page.svelte
@@ -3,6 +3,7 @@
 
 	import Tome_Detail from '$lib/Tome_Detail.svelte';
 	import Color_Scheme_Input from '$lib/Color_Scheme_Input.svelte';
+	import Style_Variable_Button from '$routes/Style_Variable_Button.svelte';
 	import {get_tome} from '$lib/tome.js';
 
 	const LIBRARY_ITEM_NAME = 'light and shadow';
@@ -14,9 +15,14 @@
 	<div class="prose">
 		<section>
 			<p>
-				Fuz is designed around the idea of stacking light and shadow. The goal is easy authoring
-				with simple consistent rules. Light mode starts from the perspective of a plain white
-				document, and dark mode starts from a lightless void.
+				Fuz is designed around the idea of stacking light and shadow with highlights and shading.
+				The goal is easy authoring with simple and consistent rules for various compositions and
+				states.
+			</p>
+			<p>
+				The details are not always simple inversions between light mode, which starts from plain
+				white documents, and dark mode, which starts from a lightless void. It's more helpful to
+				think in terms of additive light than opaque colors.
 			</p>
 			<p>
 				Opacity is used to enable arbitrary stacking that visually inherits its context. Not all
@@ -26,13 +32,14 @@
 		</section>
 
 		<section>
-			<h3><code>darken</code> and <code>lighten</code></h3>
+			<h3>Shades and highlights</h3>
+			<h4><code>darken</code> and <code>lighten</code></h4>
 			<div class="swatch">
 				{#each {length: 9} as _, i}
 					{@const name = 'darken_' + (i + 1)}
 					<div>
 						<div class="color" style:background-color="var(--{name})"></div>
-						<small>{name}</small>
+						<small><Style_Variable_Button {name} /></small>
 					</div>
 				{/each}
 			</div>
@@ -41,14 +48,14 @@
 					{@const name = 'lighten_' + (i + 1)}
 					<div>
 						<div class="color" style:background-color="var(--{name})"></div>
-						<small>{name}</small>
+						<small><Style_Variable_Button {name} /></small>
 					</div>
 				{/each}
 			</div>
 		</section>
 
 		<section>
-			<h3><code>bg</code> and <code>fg</code></h3>
+			<h4><code>bg</code> and <code>fg</code></h4>
 			<p>
 				In light mode, <code>bg</code> is the same as <code>lighten</code> and <code>fg</code> is
 				the same as <code>darken</code>. In dark mode, they're swapped.
@@ -58,7 +65,7 @@
 					{@const name = 'bg_' + (i + 1)}
 					<div>
 						<div class="color" style:background-color="var(--{name})"></div>
-						<small>{name}</small>
+						<small><Style_Variable_Button {name} /></small>
 					</div>
 				{/each}
 			</div>
@@ -67,7 +74,7 @@
 					{@const name = 'fg_' + (i + 1)}
 					<div>
 						<div class="color" style:background-color="var(--{name})"></div>
-						<small>{name}</small>
+						<small><Style_Variable_Button {name} /></small>
 					</div>
 				{/each}
 			</div>
@@ -87,7 +94,7 @@
 							style:background-color="var(--fg)"
 							style:opacity="var(--{name})"
 						></div>
-						<small>{name}</small>
+						<small><Style_Variable_Button {name} /></small>
 					</div>
 				{/each}
 			</div>
@@ -123,29 +130,39 @@
 		</section>
 		<section>
 			<h3>Shadows</h3>
-			<div class="shadow" style:box-shadow="var(--shadow)">shadow</div>
-			<div class="shadow" style:box-shadow="var(--shadow_lg)">shadow_lg</div>
-			<div class="shadow" style:box-shadow="var(--shadow_inset)">shadow_inset</div>
-			<div class="shadow" style:box-shadow="var(--shadow_inset_active)">shadow_inset_active</div>
+			<div class="shadow" style:box-shadow="var(--shadow)">
+				<Style_Variable_Button name="shadow" />
+			</div>
+			<div class="shadow" style:box-shadow="var(--shadow_lg)">
+				<Style_Variable_Button name="shadow_lg" />
+			</div>
+			<div class="shadow" style:box-shadow="var(--shadow_inset)">
+				<Style_Variable_Button name="shadow_inset" />
+			</div>
+			<div class="shadow" style:box-shadow="var(--shadow_inset_active)">
+				<Style_Variable_Button name="shadow_inset_active" />
+			</div>
 		</section>
 	</div>
 </Tome_Detail>
 
 <style>
 	.swatch {
-		display: flex;
-		align-items: center;
 		font-family: var(--font_family_mono);
-		text-align: center;
 		margin-bottom: var(--space_md);
+		display: grid;
+		grid-template-columns: repeat(auto-fill, minmax(100px, 1fr));
 	}
 	.color {
 		height: var(--input_height_sm);
-		width: 75px;
 	}
 	.shadow {
 		margin-bottom: var(--space_xl5);
 		padding: var(--space_md);
 		font-family: var(--font_family_mono);
+	}
+	small {
+		display: flex;
+		justify-content: center;
 	}
 </style>
