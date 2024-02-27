73fd2d3de2368a88c94194f9fdd9b17450e2eef0
Ryan Atkinson <mail@ryanatkn.com>
2024-02-27 16:50:36 -0700

    wip

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
 	import '$lib/style.css';
 	import '$lib/theme.css';
-	import '$lib/utility_classes.css';
+	import '$lib/animations.css';
+	import '$lib/variable_classes.css';
 	import '$lib/variable_classes.css';
 	import '@ryanatkn/fuz_code/prism.css';
 
