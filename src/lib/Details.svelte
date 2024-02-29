c2d6bdfaa393c89c5a7a2325cf21ecb991119730
Ryan Atkinson <mail@ryanatkn.com>
2024-02-29 15:28:44 -0700

    wip

---
+++ src/lib/Details.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	export let lazy = true;
+	export let ontoggle:
+		| ((e: Event & {currentTarget: EventTarget & HTMLDetailsElement}) => void)
+		| undefined = undefined;
+	export let open: boolean | null | undefined = undefined;
+</script>
+
+<details
+	bind:open
+	on:toggle={(e) => {
+		console.log(`e`, e);
+		ontoggle?.(e);
+	}}
+>
+	{#if lazy}
+		<slot />
+	{:else}
+		<slot />
+	{/if}
+</details>
