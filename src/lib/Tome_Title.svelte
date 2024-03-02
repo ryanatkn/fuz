1c8a40837bb3437f4631f4adb6ef8f1e58506f76
Ryan Atkinson <mail@ryanatkn.com>
2024-03-02 11:14:39 -0700

    wip

--- src/lib/Tome_Title.svelte
+++ src/lib/Tome_Title.svelte
@@ -1,18 +1,32 @@
 <script lang="ts">
 	import {page} from '$app/stores';
+	import type {Snippet} from 'svelte';
 
 	import type {Tome} from '$lib/tome.js';
 	import {to_library_path_info} from '$lib/helpers.js';
 
 	interface Props {
 		tome: Tome;
+		children?: Snippet;
 	}
 
-	const {tome} = $props<Props>();
+	const {tome, children} = $props<Props>();
 
 	const {path, path_is_selected} = $derived(to_library_path_info(tome.slug, $page.url.pathname));
 </script>
 
 <h2 id={tome.name}>
-	{#if path_is_selected}{tome.name}{:else}<a href={path}>{tome.name}</a>{/if}
+	{#if path_is_selected}
+		{@render content(tome.name)}
+	{:else}
+		<a href={path}>{@render content(tome.name)}</a>
+	{/if}
 </h2>
+
+{#snippet content(name: string)}
+	{#if children}
+		{@render children()}
+	{:else}
+		{name}
+	{/if}
+{/snippet}
