227cd78aa18ef70057dd756057de351b627c5da0
Ryan Atkinson <mail@ryanatkn.com>
2024-03-02 18:56:42 -0700

    wip

--- src/routes/library/classes/+page.svelte
+++ src/routes/library/classes/+page.svelte
@@ -2,6 +2,7 @@
 	import Code from '@ryanatkn/fuz_code/Code.svelte';
 
 	import Tome_Detail from '$lib/Tome_Detail.svelte';
+	import Tome_Link from '$lib/Tome_Link.svelte';
 	import {get_tome} from '$lib/tome.js';
 	import utility_classes_text from '$lib/utility_classes.css?raw';
 	import variable_classes_text from '$lib/variable_classes.css?raw';
@@ -11,19 +12,17 @@
 	const LIBRARY_ITEM_NAME = 'classes';
 
 	const tome = get_tome(LIBRARY_ITEM_NAME);
+
+	const utility_classes = ['utility'];
+	const variable_classes = ['variable'];
+	const semantic_classes = ['semantic'];
 </script>
 
 <Tome_Detail {tome}>
 	<div class="prose">
 		<section>
-			Fuz provides an arbitrary subset of utility classes for CSS, and defers to CSS in <a
-				href="https://svelte.dev/docs/svelte-components#style">Svelte's <code>{'<'}style></code></a
-			> for complex and uncommon patterns. This may change depending on build tool sophistication, like
-			a compiler or Vite plugin to remove unused styles.
-		</section>
-		<section>
 			<h3>Optional CSS classes</h3>
-			<p>Fuz has three optional CSS files with classes. To use them:</p>
+			<p>Fuz has three optional CSS files with different kinds of classes. To use them:</p>
 			<Code
 				content={`<!-- +layout.svelte -->
 <script>
@@ -35,28 +34,82 @@
 	// ...`}
 			/>
 			<p>
-				Utility classes are helpers for base CSS. Variable classes are utility classes based on Fuz
-				style variables. Semantic classes are useful groupings of styles that are like the CSS
-				equivalent of Svelte components.
+				Utility classes use plain CSS for convenient HTML authoring. Variable classes are utility
+				classes based on Fuz style <Tome_Link name="variables" />. Semantic classes are named
+				groupings of styles that are like the CSS equivalent of Svelte components.
 			</p>
+			<aside>
+				With more sophisticated build tooling, these classes could be generated on demand instead of
+				distributed as static files, or unused classes could be removed with a Vite plugin.
+			</aside>
 		</section>
 		<section>
-			<h3>Utility classees</h3>
+			<h3>Utility classes</h3>
+			<p>
+				Fuz provides an arbitrary set of utility classes for CSS, and defers to CSS in <a
+					href="https://svelte.dev/docs/svelte-components#style"
+					>Svelte's <code>{'<'}style></code></a
+				> for complex and uncommon patterns.
+			</p>
+			<div class="classes">
+				{#each utility_classes as c}
+					<div>{c}</div>
+				{/each}
+			</div>
+		</section>
+		<section>
+			<h3>Variable classes</h3>
+			<div class="classes">
+				{#each variable_classes as c}
+					<div>{c}</div>
+				{/each}
+			</div>
+		</section>
+		<section>
+			<h3>Semantic classes</h3>
+			<div class="classes">
+				{#each semantic_classes as c}
+					<div>{c}</div>
+				{/each}
+			</div>
 		</section>
 		<section>
 			<h3>Raw CSS</h3>
+			<!-- TODO maybe add a github source link? -->
 			<Details>
-				<summary slot="summary">utility_classes.css</summary>
+				<summary slot="summary"
+					><span class="font_family_mono">@ryanatkn/fuz/utility_classes.css</span></summary
+				>
+				<p class="mb_lg">
+					<a href="https://github.com/ryanatkn/fuz/blob/main/src/lib/utility_classes.css">source</a>
+				</p>
 				<Code content={utility_classes_text} lang="css" />
 			</Details>
 			<Details>
-				<summary slot="summary">variable_classes.css</summary>
+				<summary slot="summary"
+					><span class="font_family_mono">@ryanatkn/fuz/variable_classes.css</span></summary
+				>
+				<p class="mb_lg">
+					<a href="https://github.com/ryanatkn/fuz/blob/main/src/lib/variable_classes.css">source</a
+					>
+				</p>
 				<Code content={variable_classes_text} lang="css" />
 			</Details>
 			<Details>
-				<summary slot="summary">semantic_classes.css</summary>
+				<summary slot="summary"
+					><span class="font_family_mono">@ryanatkn/fuz/semantic_classes.css</span></summary
+				>
+				<p class="mb_lg">
+					<a href="https://github.com/ryanatkn/fuz/blob/main/src/lib/semantic_classes.css">source</a
+					>
+				</p>
 				<Code content={semantic_classes_text} lang="css" />
 			</Details>
 		</section>
 	</div>
 </Tome_Detail>
+
+<style>
+	.classes {
+	}
+</style>
