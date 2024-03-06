498105848d7388e1e02c48eedfbb03b288636431
Ryan Atkinson <mail@ryanatkn.com>
2024-03-06 08:36:24 -0700

    wip

--- src/routes/library/Breadcrumb/Breadcrumb_Example.svelte
+++ src/routes/library/Breadcrumb/Breadcrumb_Example.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	import Code from '@ryanatkn/fuz_code/Code.svelte';
+
 	import Breadcrumb from '$lib/Breadcrumb.svelte';
 
 	interface Props {
@@ -8,7 +10,17 @@
 	const {selected_path} = $props<Props>();
 </script>
 
+<Code
+	content={`<Breadcrumb
+	path="/a/b/c"
+	selected_path="${selected_path}"
+	base_path="/library/Breadcrumb"	
+>
+	<span class="size_xl">🔡</span>
+	{#snippet separator()}.{/snippet}
+</Breadcrumb>`}
+/>
 <Breadcrumb path="/a/b/c" {selected_path} base_path="/library/Breadcrumb">
 	<span class="size_xl">🔡</span>
-	<div slot="separator">.</div>
+	{#snippet separator()}.{/snippet}
 </Breadcrumb>
