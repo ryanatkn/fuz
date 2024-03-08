3acc4a4c49fa5ba789010e51e7bd0bffb6831a38
Ryan Atkinson <mail@ryanatkn.com>
2024-03-08 11:17:04 -0700

    wip

--- src/lib/Tome_Link.svelte
+++ src/lib/Tome_Link.svelte
@@ -1,19 +1,23 @@
 <script lang="ts">
 	import {base} from '$app/paths';
+	import type {Snippet} from 'svelte';
 	import type {HTMLAnchorAttributes} from 'svelte/elements';
 
 	interface Props {
 		name: string; // TODO type, generate from `tomes`?
 		chip?: boolean;
 		attrs?: HTMLAnchorAttributes;
+		children?: Snippet;
 	}
 
-	const {name, chip = true, attrs}: Props = $props();
+	const {name, chip = true, attrs, children}: Props = $props();
 
 	// TODO add contextmenu behavior
 </script>
 
-<a {...attrs} class:chip href="{base}/library/{name}"><slot>{name}</slot></a>
+<a {...attrs} class:chip href="{base}/library/{name}"
+	>{#if children}{@render children()}{:else}{name}{/if}</a
+>
 
 <style>
 	a {
