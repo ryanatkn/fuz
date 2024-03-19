08544ec01b309d5021e7bfc2a7d9345ed8ff409b
Ryan Atkinson <mail@ryanatkn.com>
2024-03-19 16:38:01 -0600

    wip

--- src/routes/library/Contextmenu/Example_Default_Behaviors.svelte
+++ src/routes/library/Contextmenu/Example_Default_Behaviors.svelte
@@ -7,30 +7,28 @@
 </script>
 
 <Contextmenu_Root scoped>
-	<div class="prose">
-		<h3>Default behaviors example</h3>
-		<div class="panel p_md">
-			<Code
-				content={`<Contextmenu_Root scoped>
+	<h3>Default behaviors example</h3>
+	<div class="panel p_md">
+		<Code
+			content={`<Contextmenu_Root scoped>
   ...<a href="https://www.fuz.dev/">
     a link like this one
   </a>...
 </Contextmenu_Root>`}
-			/>
-			<p>
-				Opening the contextmenu on <a href="https://www.fuz.dev/">a link like this one</a> has
-				special behavior by default. To accesss your browser's normal contextmenu, open the
-				contextmenu on the link inside the contextmenu itself or hold <code>Shift</code>.
-			</p>
-			<p>
-				Although disruptive to default browser behavior, this allows links to have contextmenu
-				behaviors, and it allows you to open the contextmenu anywhere to access all contextual
-				behaviors.
-			</p>
-			<aside>
-				Notice that opening the contextmenu on anything here except the link passes through to the
-				browser's default contextmenu, because we didn't include any behaviors.
-			</aside>
-		</div>
+		/>
+		<p>
+			Opening the contextmenu on <a href="https://www.fuz.dev/">a link like this one</a> has special
+			behavior by default. To accesss your browser's normal contextmenu, open the contextmenu on the
+			link inside the contextmenu itself or hold <code>Shift</code>.
+		</p>
+		<p>
+			Although disruptive to default browser behavior, this allows links to have contextmenu
+			behaviors, and it allows you to open the contextmenu anywhere to access all contextual
+			behaviors.
+		</p>
+		<aside>
+			Notice that opening the contextmenu on anything here except the link passes through to the
+			browser's default contextmenu, because we didn't include any behaviors.
+		</aside>
 	</div>
 </Contextmenu_Root>
