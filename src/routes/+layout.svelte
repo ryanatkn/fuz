5d1e23279716f85df38759a1d6e0b1d0639888ca
Ryan Atkinson <mail@ryanatkn.com>
2024-03-24 11:48:47 -0600

    wip

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,6 @@
 <script lang="ts">
 	import '$lib/style.css';
 	import '$lib/theme.css';
-	import '$lib/utility_classes.css';
-	import '$lib/semantic_classes.css';
-	import '$lib/animations.css';
 	import '@ryanatkn/fuz_code/prism.css';
 
 	import Themed from '$lib/Themed.svelte';
