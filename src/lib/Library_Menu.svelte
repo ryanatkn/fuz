8d8d9a9e1585c28aaf763d7cf0bd9d7e47a794ef
Ryan Atkinson <mail@ryanatkn.com>
2024-04-04 18:59:48 -0600

    wip

--- src/lib/Library_Menu.svelte
+++ src/lib/Library_Menu.svelte
@@ -4,7 +4,6 @@
 	import {base} from '$app/paths';
 
 	import type {Tome} from '$lib/tome.js';
-	import Library_Panel from '$lib/Library_Panel.svelte';
 	import type {Snippet} from 'svelte';
 
 	interface Props {
@@ -30,34 +29,36 @@
 	// TODO BLOCK remove CSS below
 </script>
 
-{#each Object.entries(tomes_by_category) as [category, tomes] (category)}
-	<div transition:slide>
-		<Library_Panel>
-			<div class="library_menu">
-				{#if children}{@render children(category)}{:else}<h6>{category}</h6>{/if}
-				<menu class="unstyled">
-					{#each tomes as item (item.slug)}
-						<li role="none" transition:slide>
-							<a
-								class="menu_item"
-								href="{base}/library/{item.slug}"
-								class:selected={item.pathname === $page.url.pathname}>{item.name}</a
-							>
-						</li>
-					{/each}
-				</menu>
-			</div>
-		</Library_Panel>
-	</div>
-{/each}
+<ul class="unstyled">
+	{#each Object.entries(tomes_by_category) as [category, tomes] (category)}
+		<li class="category">
+			{#if children}{@render children(category)}{:else}<h6>{category}</h6>{/if}
+			<ul class="unstyled">
+				{#each tomes as item (item.slug)}
+					<li role="none" transition:slide>
+						<a
+							class="menu_item"
+							href="{base}/library/{item.slug}"
+							class:selected={item.pathname === $page.url.pathname}>{item.name}</a
+						>
+					</li>
+				{/each}
+			</ul>
+		</li>
+	{/each}
+</ul>
 
 <style>
+	.category {
+		margin: var(--space_xl6) 0;
+	}
+
 	/* TODO should be a CSS class or variable, maybe should be the default?
 	problem is it doesn't work on .bg, maybe needs a variant/modifier in the name? */
-	menu a:hover {
+	ul a:hover {
 		background-color: var(--bg_5);
 	}
-	menu a:is(:active, .selected) {
+	ul a:is(:active, .selected) {
 		background-color: var(--bg_7);
 	}
 </style>
--- src/lib/Library_Panel.svelte
+++ src/lib/Library_Panel.svelte
@@ -6,6 +6,8 @@
 	}
 
 	const {children}: Props = $props();
+
+	// TODO BLOCK maybe delete this component
 </script>
 
 <div class="library_panel width_md">
