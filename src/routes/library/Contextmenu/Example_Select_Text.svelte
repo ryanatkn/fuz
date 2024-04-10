fb121fc85f12f549deaea57ac39308e3945908f9
Ryan Atkinson <mail@ryanatkn.com>
2024-04-10 07:23:43 -0600

    wip

--- src/routes/library/Contextmenu/Example_Select_Text.svelte
+++ src/routes/library/Contextmenu/Example_Select_Text.svelte
@@ -73,12 +73,10 @@ Try selecting text and then opening the contextmenu on it.`;
 				<Mdn_Link href="Web/HTML/Global_attributes/contenteditable">contenteditable</Mdn_Link> likewise
 				has your browser's default contextmenu behavior.
 			</p>
-			<p><code>contenteditable="true"</code></p>
-			<blockquote contenteditable="true" bind:innerText={value} />
+			<p><code>contenteditable</code></p>
+			<blockquote contenteditable bind:innerText={value} />
 			<p><code>contenteditable="plaintext-only"</code></p>
 			<blockquote contenteditable="plaintext-only" bind:innerText={value} />
-			<p><code>contenteditable</code> (same as empty string)</p>
-			<blockquote contenteditable bind:innerText={value} />
 		</div>
 	</Contextmenu>
 </Contextmenu_Root>
