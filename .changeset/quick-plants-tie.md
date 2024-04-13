d55f83b61ec54a19a7f22c1ce1624c0457818d2d
Ryan Atkinson <mail@ryanatkn.com>
2024-04-13 16:38:22 -0600

    publish v0.96.0

--- .changeset/quick-plants-tie.md
+++
@@ -1,5 +0,0 @@
----
-"@ryanatkn/fuz": minor
----
-
-rename `Spider` from `Fuz_Logo` and publish `Fuz_Logo` and `Spiders`
--- src/routes/package.ts
+++ src/routes/package.ts
@@ -5,7 +5,7 @@ import type {Src_Json} from '@ryanatkn/gro/src_json.js';
 
 export const package_json = {
 	name: '@ryanatkn/fuz',
-	version: '0.95.0',
+	version: '0.96.0',
 	description: 'Svelte UI library and CSS design system',
 	icon: '🧶',
 	public: true,
@@ -311,7 +311,7 @@ export const package_json = {
 
 export const src_json = {
 	name: '@ryanatkn/fuz',
-	version: '0.95.0',
+	version: '0.96.0',
 	modules: {
 		'./package.json': {path: 'package.json', declarations: []},
 		'./Alert.svelte': {path: 'Alert.svelte', declarations: []},
