cf5816f39dd0520139f029e3dc5598e347827f62
Ryan Atkinson <mail@ryanatkn.com>
2024-06-10 17:46:13 -0600

    wip

--- src/lib/Pending_Button.svelte
+++ src/lib/Pending_Button.svelte
@@ -10,27 +10,20 @@
 		title?: string;
 		disabled?: boolean;
 		attrs?: any;
+		el?: HTMLButtonElement | undefined;
 		children: Snippet;
 	}
 
-	const {pending, onclick, running, title, disabled, attrs, children}: Props = $props();
-
-	let el: HTMLButtonElement | undefined; // intentionally not `$state`, would need to use `untrack` in the helpers below if changed
-
-	/**
-	 * This API pattern is an experiment in contrast to exposing `el` through a bindable prop.
-	 * This is non-reactive, which I believe is generally what consumers want,
-	 * but this forces the issue which may cause issues.
-	 *
-	 * The initial motivation is to avoid grossness with `prefer-const` and `$props`,
-	 * which is admittedly not a good motivation to shape the API.
-	 * A better fix would be to have the ESLint plugin disable `prefer-const` for `$props`,
-	 * or perhaps that's something for the language tools to handle internally by, for example,
-	 * making each `$prop` a separate declaration that defaults to `const` for non-bindables.
-	 */
-	export const get_el = (): HTMLButtonElement | undefined => el;
-
-	export const focus = (): void => el?.focus();
+	let {
+		pending, // eslint-disable-line prefer-const
+		onclick, // eslint-disable-line prefer-const
+		running, // eslint-disable-line prefer-const
+		title, // eslint-disable-line prefer-const
+		disabled, // eslint-disable-line prefer-const
+		attrs, // eslint-disable-line prefer-const
+		el = $bindable(),
+		children, // eslint-disable-line prefer-const
+	}: Props = $props();
 
 	// TODO maybe this shouldn't disable? just visually look disabled, maybe with `.disabled`?
 	// TODO cancelable?
