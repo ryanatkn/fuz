5647ac324964d7b1df6f145b57b66d6a4c0347c5
Ryan Atkinson <mail@ryanatkn.com>
2024-06-24 07:39:26 -0600

    downgrade `svelte@5.0.0-next.164` to fix dist types

---
+++ .changeset/quiet-snails-add.md
@@ -0,0 +1,5 @@
+---
+"@ryanatkn/fuz": patch
+---
+
+downgrade `svelte@5.0.0-next.164` to fix dist types
--- src/routes/package.ts
+++ src/routes/package.ts
@@ -53,7 +53,7 @@ export const package_json = {
 		'prettier-plugin-svelte': '^3.2.5',
 		'prism-svelte': '^0.5.0',
 		prismjs: '^1.29.0',
-		svelte: '^5.0.0-next.165',
+		svelte: '^5.0.0-next.164',
 		'svelte-check': '^3.8.1',
 		tslib: '^2.6.3',
 		typescript: '^5.5.2',
