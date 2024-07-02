c9f076b8e7cf4a7ce20e4b9a13ac0050cad5e611
Ryan Atkinson <mail@ryanatkn.com>
2024-07-02 16:36:32 -0600

    wip

--- src/routes/library/introduction/+page.svelte
+++ src/routes/library/introduction/+page.svelte
@@ -9,10 +9,25 @@
 
 <Tome_Detail {tome}>
 	<section>
-		<blockquote>
-			⚠️ Docs are a work in progress. <a href="https://moss.ryanatkn.com/">Moss</a> was recently
-			extracted from Fuz, and these docs are incomplete and misleading in some cases. For an
-			overview see <a href="https://github.com/ryanatkn/fuz#readme">the readme</a>.
-		</blockquote>
+		<p>
+			Fuz is a Svelte UI library that builds on my CSS framework <a
+				href="https://moss.ryanatkn.com/">Moss</a
+			>. It's in early alpha and there will be many breaking changes. I'm making it to support
+			<a href="https://zzz.ryanatkn.com/">Zzz</a>
+			and
+			<a href="https://www.ryanatkn.com/">my other projects</a> that focus on end-users, so it'll grow
+			slowly as I find the time and usecases.
+		</p>
+		<p>
+			These docs are a work in progress - for an overview see
+			<a href="https://github.com/ryanatkn/fuz#readme">the readme</a>.
+		</p>
+		<p>
+			The goal is to make a fully-featured Svelte UI library, but it'll take extra time to get
+			there. That lets us prioritize patterns like APIs above features in the meantime, making it an
+			interesting project for learning and experimentation. Feel free to take the code and ideas for
+			your own purposes. If you're interested in using it or helping out, visit
+			<a href="https://discord.gg/YU5tyeK72X">the Discord</a>.
+		</p>
 	</section>
 </Tome_Detail>
