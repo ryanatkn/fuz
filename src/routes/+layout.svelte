8fa350df923fcab35c76cd9ea06b09f65f622026
Ryan Atkinson <mail@ryanatkn.com>
2024-07-05 11:11:38 -0600

    wip

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,8 +5,6 @@
 
 	import {parse_package_meta} from '@ryanatkn/gro/package_meta.js';
 	import type {Snippet} from 'svelte';
-	import Prism from 'prismjs';
-	Prism; // TODO why is this import needed? Is there a Vite config option that would be better?
 
 	import Themed from '$lib/Themed.svelte';
 	import {set_pkg} from '$routes/pkg.js';
