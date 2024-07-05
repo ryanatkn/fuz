5d8254a1ea5fd1b4133fd3900cdd733590077793
Ryan Atkinson <mail@ryanatkn.com>
2024-07-05 11:34:51 -0600

    wip

--- src/routes/library/Dialog/+page.svelte
+++ src/routes/library/Dialog/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import {slide} from 'svelte/transition';
 	import {writable} from 'svelte/store';
-	// TODO @multiple why is this import needed? `Code` already imports it. Fails in dev with SSR enabled without it. I there a Vite config option that would be better? I tried the combinations of `ssr.external/noExternal` with `@ryanatkn/fuz_code` and `prismjs`.
+	// TODO @multiple why is this import needed? `Code` already imports it. Fails in dev with SSR enabled without it. Is there a Vite config option that would be better? I tried the combinations of `ssr.external/noExternal/external` with `@ryanatkn/fuz_code` and `prismjs`.
 	import Prism from 'prismjs';
 	Prism;
 	import Code from '@ryanatkn/fuz_code/Code.svelte';
--- src/routes/library/Themed/+page.svelte
+++ src/routes/library/Themed/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import {writable} from 'svelte/store';
-	// TODO @multiple why is this import needed? `Code` already imports it. Fails in dev with SSR enabled without it. I there a Vite config option that would be better? I tried the combinations of `ssr.external/noExternal` with `@ryanatkn/fuz_code` and `prismjs`.
+	// TODO @multiple why is this import needed? `Code` already imports it. Fails in dev with SSR enabled without it. Is there a Vite config option that would be better? I tried the combinations of `ssr.external/noExternal/external` with `@ryanatkn/fuz_code` and `prismjs`.
 	import Prism from 'prismjs';
 	Prism;
 	import Code from '@ryanatkn/fuz_code/Code.svelte';
