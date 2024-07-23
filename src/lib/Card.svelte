6517d8c6ceb34322201b0fcaf58aeb9b2e954bbe
Ryan Atkinson <mail@ryanatkn.com>
2024-07-22 21:04:49 -0600

    fix card shadows

--- src/lib/Card.svelte
+++ src/lib/Card.svelte
@@ -86,10 +86,10 @@
 		text-align: center;
 	}
 	.link {
-		box-shadow: var(--shadow, var(--shadow_outset_sm));
+		box-shadow: var(--shadow, var(--shadow_outset_sm) var(--shadow_color_sm));
 	}
 	.link:active {
-		box-shadow: var(--shadow, var(--shadow_inset_sm));
+		box-shadow: var(--shadow, var(--shadow_inset_sm) var(--shadow_color_sm));
 	}
 	.link.selected .content,
 	.link:hover .content {
