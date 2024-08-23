1b2e86185c75711e5433d85daba7556b9590aac9
Ryan Atkinson <mail@ryanatkn.com>
2024-08-23 08:27:05 -0600

    wip

--- src/lib/Spiders.svelte
+++ src/lib/Spiders.svelte
@@ -1,4 +1,4 @@
-<script context="module" lang="ts">
+<script module lang="ts">
 	/**
 	 * Returns the minute of the current day.
 	 */
--- src/lib/Tome_Subheading.svelte
+++ src/lib/Tome_Subheading.svelte
@@ -1,4 +1,4 @@
-<script lang="ts" context="module">
+<script lang="ts" module>
 	let _id = 0;
 </script>
 
