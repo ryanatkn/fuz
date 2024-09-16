2f3a98a13ea045bee2d613033356ab1ceeb49f00
Ryan Atkinson <mail@ryanatkn.com>
2024-09-16 15:06:50 -0600

    allow library to scale better (#52)

---
+++ .changeset/twenty-poems-leave.md
@@ -0,0 +1,5 @@
+---
+'@ryanatkn/fuz': minor
+---
+
+add snippets to `Library_Content` to customize the root page and remove the `root_path` prop
--- src/lib/Library_Content.svelte
+++ src/lib/Library_Content.svelte
@@ -1,6 +1,4 @@
 <script lang="ts">
-	import {page} from '$app/stores';
-	import {base} from '$app/paths';
 	import type {Package_Meta} from '@ryanatkn/gro/package_meta.js';
 	import type {Snippet} from 'svelte';
 
@@ -8,28 +6,29 @@
 	import Package_Summary from '$lib/Package_Summary.svelte';
 
 	interface Props {
-		root_path?: string;
 		tomes: Tome[];
 		pkg: Package_Meta;
 		repo_name?: Snippet<[repo_name: string]>;
+		content?: Snippet;
+		header?: Snippet;
 	}
 
-	const {root_path = '/library', tomes, pkg, repo_name}: Props = $props();
+	const {tomes, pkg, repo_name, content, header}: Props = $props();
 </script>
 
-{#if $page.url.pathname === base + root_path}
+{#if header}
+	{@render header()}
+{:else}
 	<div class="box mb_xl5">
 		<Package_Summary {pkg} {repo_name} />
 	</div>
 {/if}
-<div class="tomes">
-	{#each tomes as tome (tome)}
-		<tome.component />
-	{/each}
+<div>
+	{#if content}
+		{@render content()}
+	{:else}
+		{#each tomes as tome (tome)}
+			<tome.component />
+		{/each}
+	{/if}
 </div>
-
-<style>
-	.tomes {
-		width: 100%;
-	}
-</style>
