3bbabeb43fc9885fea264f4fc8c75bee68e2d633
Ryan Atkinson <mail@ryanatkn.com>
2024-09-20 14:14:29 -0600

    wip

--- src/lib/Contextmenu_Submenu.svelte
+++ src/lib/Contextmenu_Submenu.svelte
@@ -78,7 +78,7 @@
 	>
 		<div class="content">
 			<div class="icon">
-				{#if icon}{@render icon()}{/if}
+				{@render icon?.()}
 			</div>
 			<div class="title">{@render children()}</div>
 		</div>
