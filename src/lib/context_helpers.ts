2dce5921e50c2f3a51c50166fcf91718d39117b5
Ryan Atkinson <mail@ryanatkn.com>
2024-09-22 00:41:45 -0600

    wip

--- src/lib/context_helpers.ts
+++ src/lib/context_helpers.ts
@@ -57,3 +57,59 @@ export class Svelte_Context<T> {
 		return value;
 	}
 }
+
+// TODO BLOCK this instead?
+
+// This uses a function instead of a class because of the overloaded type signatures.
+
+/**
+ * Wraps Svelte's `setContext` and `getContext` for better ergonmics.
+ *
+ * If a `fallback` is provided, `optional` is implicitly `false`.
+ *
+ * If `optional` is `false` or `undefined` and no `fallback` is provided,
+ * `get` throws an error if no value is set in the context.
+ */
+export function create_context<T>(options: {label?: string; fallback: () => T}): {
+	get: () => T;
+	set: (value: T | undefined) => T;
+};
+export function create_context<T>(options: {label?: string; optional?: false}): {
+	get: (message?: string) => T;
+	set: (value: T) => T;
+};
+export function create_context<T>(options: {label?: string; optional: true}): {
+	get: () => T | undefined;
+	set: (value: T) => T;
+};
+export function create_context<T>(options: {
+	label?: string;
+	fallback?: () => T;
+	optional?: boolean;
+}):
+	| {get: (message?: string) => T; set: (value: T | undefined) => T}
+	| {get: (message?: string) => T | undefined; set: (value: T | undefined) => T} {
+	const {label, fallback, optional} = options;
+	const key = Symbol(label);
+	return {
+		get: (message?: string) => {
+			const found: T | undefined = getContext(key); // `null` is a valid value
+			const value = found === undefined ? fallback?.() : found; // eslint-disable-line @typescript-eslint/prefer-nullish-coalescing
+			if (!optional && value === undefined) {
+				throw Error(message ?? 'context value not set' + (label ? ` for "${label}"` : ''));
+			}
+			return value;
+		},
+		set(value: T | undefined = fallback?.()): T {
+			if (value === undefined) {
+				throw Error(
+					'context value' +
+						(label ? ` "${label}"` : '') +
+						' is not defined - provide a value to `set` or fallback in the constructor',
+				);
+			}
+			setContext(key, value);
+			return value;
+		},
+	};
+}
