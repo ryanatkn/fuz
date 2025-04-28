752730967598d04654052f1bd10462c78afffd74
Ryan Atkinson <mail@ryanatkn.com>
2025-04-28 15:56:00 -0600

    wip

--- src/lib/csp.ts
+++ src/lib/csp.ts
@@ -2,7 +2,6 @@
 
 import type {Array_Element, Defined} from '@ryanatkn/belt/types.js';
 
-import {csp_directive_spec_by_name, csp_directive_specs} from '$lib/csp_directive_specs.js';
 import {deep_freeze} from '$lib/helpers.js';
 
 // TODO schemas, but I may be moving to ArkType from Zod if precompilation looks good
@@ -297,6 +296,157 @@ export const csp_directive_required_trust_defaults: Record<Csp_Directive, Csp_Tr
 		'trusted-types': null,
 		sandbox: null,
 	});
+
+/**
+ * Static data descriptors for the CSP directives.
+ * Fuz excludes deprecated directives, so those are intentionally omitted,
+ * but any newer missing directives are bugs.
+ *
+ * Could be moved to `csp.ts` but is currently here to keep that module smaller.
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Headers/Content-Security-Policy
+ */
+export const csp_directive_specs: Array<Csp_Directive_Spec> = [
+	{
+		name: 'default-src',
+		fallback: null,
+		fallback_of: [
+			'script-src',
+			'script-src-elem',
+			'script-src-attr',
+			'style-src',
+			'style-src-elem',
+			'style-src-attr',
+			'img-src',
+			'media-src',
+			'font-src',
+			'manifest-src',
+			'child-src',
+			'connect-src',
+			'worker-src',
+			'object-src',
+		],
+	},
+	{
+		name: 'script-src',
+		fallback: ['default-src'],
+		fallback_of: ['script-src-elem', 'script-src-attr', 'worker-src'],
+	},
+	{
+		name: 'script-src-elem',
+		fallback: ['script-src', 'default-src'],
+		fallback_of: null,
+	},
+	{
+		name: 'script-src-attr',
+		fallback: ['script-src', 'default-src'],
+		fallback_of: null,
+	},
+	{
+		name: 'style-src',
+		fallback: ['default-src'],
+		fallback_of: ['style-src-elem', 'style-src-attr'],
+	},
+	{
+		name: 'style-src-elem',
+		fallback: ['style-src', 'default-src'],
+		fallback_of: null,
+	},
+	{
+		name: 'style-src-attr',
+		fallback: ['style-src', 'default-src'],
+		fallback_of: null,
+	},
+	{
+		name: 'img-src',
+		fallback: ['default-src'],
+		fallback_of: null,
+	},
+	{
+		name: 'media-src',
+		fallback: ['default-src'],
+		fallback_of: null,
+	},
+	{
+		name: 'font-src',
+		fallback: ['default-src'],
+		fallback_of: null,
+	},
+	{
+		name: 'manifest-src',
+		fallback: ['default-src'],
+		fallback_of: null,
+	},
+	{
+		name: 'child-src',
+		fallback: ['default-src'],
+		fallback_of: ['frame-src', 'worker-src'],
+	},
+	{
+		name: 'connect-src',
+		fallback: ['default-src'],
+		fallback_of: null,
+	},
+	{
+		name: 'frame-src',
+		fallback: ['child-src'],
+		fallback_of: null,
+	},
+	{
+		name: 'frame-ancestors',
+		fallback: null,
+		fallback_of: null,
+	},
+	{
+		name: 'form-action',
+		fallback: null,
+		fallback_of: null,
+	},
+	{
+		name: 'worker-src',
+		fallback: ['child-src', 'script-src', 'default-src'],
+		fallback_of: null,
+	},
+	{
+		name: 'object-src',
+		fallback: ['default-src'],
+		fallback_of: null,
+	},
+	{
+		name: 'base-uri',
+		fallback: null,
+		fallback_of: null,
+	},
+	{
+		name: 'upgrade-insecure-requests',
+		fallback: null,
+		fallback_of: null,
+	},
+	{
+		name: 'report-to',
+		fallback: null,
+		fallback_of: null,
+	},
+	{
+		name: 'require-trusted-types-for',
+		fallback: null,
+		fallback_of: null,
+	},
+	{
+		name: 'trusted-types',
+		fallback: null,
+		fallback_of: null,
+	},
+	{
+		name: 'sandbox',
+		fallback: null,
+		fallback_of: null,
+	},
+];
+
+export const csp_directive_spec_by_name: Map<Csp_Directive, Csp_Directive_Spec> = new Map(
+	csp_directive_specs.map((s) => [s.name, s]),
+);
 
 // CSP types from SvelteKit, adapted with changes from @sveltejs/kit/src/types/private.d.ts
 //
