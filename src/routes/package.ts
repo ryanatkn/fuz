24daadf652aa60ed12f2767af3f354384d91320a
Ryan Atkinson <mail@ryanatkn.com>
2025-10-22 06:56:26 -0400

    wip

--- src/routes/package.ts
+++ src/routes/package.ts
@@ -248,7 +248,6 @@ export const src_json: Src_Json = {
 				{name: 'create_keyboard_event', kind: 'function'},
 				{name: 'create_mouse_event', kind: 'function'},
 				{name: 'set_event_target', kind: 'function'},
-				{name: 'flush_updates', kind: 'function'},
 				{name: 'create_touch_event', kind: 'function'},
 			],
 		},
